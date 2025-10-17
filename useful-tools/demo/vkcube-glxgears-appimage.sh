325a203f75587ae4b42d4d7c229bf9a63ee51eaf
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-17 03:27:06 -0400

    no zsync

--- useful-tools/demo/vkcube-glxgears-appimage.sh
+++ useful-tools/demo/vkcube-glxgears-appimage.sh
@@ -48,6 +48,6 @@ wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 ./uruntime2appimage
 mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
+mv -v ./*.AppImage ./dist
 
 echo "All Done!"
