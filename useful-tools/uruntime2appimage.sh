2590fe37bf5f69b69b37c6bb1fde82f1edf757ab
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-08 06:03:55 -0400

    update uruntime link

--- useful-tools/uruntime2appimage.sh
+++ useful-tools/uruntime2appimage.sh
@@ -13,7 +13,7 @@ ARCH=${ARCH:-$(uname -m)}
 APPDIR=${APPDIR:-$PWD/AppDir}
 OUTPATH=${OUTPATH:-$PWD}
 DWARFS_COMP="${DWARFS_COMP:-zstd:level=22 -S26 -B6}"
-URUNTIME_LINK=${URUNTIME_LINK:-https://github.com/VHSgunzo/uruntime/releases/download/v0.5.0/uruntime-appimage-dwarfs-lite-$ARCH}
+URUNTIME_LINK=${URUNTIME_LINK:-https://github.com/VHSgunzo/uruntime/releases/download/v0.5.1/uruntime-appimage-dwarfs-lite-$ARCH}
 DWARFS_LINK=${DWARFS_LINK:-https://github.com/mhx/dwarfs/releases/download/v0.14.1/dwarfs-universal-0.14.1-Linux-$ARCH}
 TMPDIR=${TMPDIR:-/tmp}
 DWARFS_CMD="${DWARFS_CMD:-$TMPDIR/mkdwarfs}"
