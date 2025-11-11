b8c7fb43a1fdc3857fa6865d30d7a8443f087d97
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-10 21:50:21 -0400

    remove quotes
    this fails if `SUDOCMD` is set to empty

--- useful-tools/get-debloated-pkgs.sh
+++ useful-tools/get-debloated-pkgs.sh
@@ -293,7 +293,7 @@ else
 	SUDOCMD=""
 fi
 
-"$SUDOCMD" pacman -U --noconfirm "$TMPDIR"/*
+$SUDOCMD pacman -U --noconfirm "$TMPDIR"/*
 
 # the gdk-pixbuf2 package needs to have the loaders.cache regenerated
 if [ -f "$TMPDIR"/gdk-pixbuf2* ] && [ -x /usr/bin/gdk-pixbuf-query-loaders ]; then
