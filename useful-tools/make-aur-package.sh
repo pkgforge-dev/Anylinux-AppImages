d8e0665d302361b3e18835d0b08cb8c0e258d8ff
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-22 18:10:18 -0400

    refactor, build local PKGBUILD when no args are provided (#191)

--- useful-tools/make-aur-package.sh
+++ useful-tools/make-aur-package.sh
@@ -18,59 +18,94 @@ _info_msg() {
 	printf "$g%s$o\n" "$l" "${*:-$l}" "$l"
 }
 
-# check for basic build dependencies
-for d in base-devel git; do
-	if ! pacman -Q "$d" 2>/dev/null; then
-		_info_msg "Adding build dependency: $d"
-		pacman -Syu --noconfirm "$d"
-	fi
-done
-
-# makepkg does not run when root
-sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-sed -i \
-	-e 's|-O2|-O3|'                              \
-	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
-	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
-	/etc/makepkg.conf
-cat /etc/makepkg.conf
-
-if [ "$1" = '--chaotic-aur' ]; then
-	shift
-	pacman-key --init
-	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-	pacman-key --lsign-key 3056513887B78AEB
-	pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-	pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-	echo '[chaotic-aur]' >> /etc/pacman.conf
-	echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
-	_info_msg "Adding Chaotic AUR packages: $*"
-	pacman -Sy --noconfirm "$@"
-	exit 0
-elif [ "$1" = '--archlinux-pkg' ]; then
-	shift
-	git clone --depth 1 https://gitlab.archlinux.org/archlinux/packaging/packages/"$1" ./"$1"
-else
-	git clone --depth 1 https://aur.archlinux.org/"$1" ./"$1"
-fi
-cd ./"$1"
-
-if ! grep -q "arch=.*$ARCH" ./PKGBUILD; then
-	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-fi
-
-# Run extra commands from env var
-if [ -n "$PRE_BUILD_CMDS" ]; then
-	_info_msg "Running additional pre-build commands..."
-	while IFS= read -r CMD; do
-		if [ -n "$CMD" ]; then
-			_info_msg "Running: $CMD"
-			eval "$CMD"
+_prepare() {
+	for d in base-devel git; do
+		if ! pacman -Q "$d" 2>/dev/null; then
+			_info_msg "Adding build dependency: $d"
+			pacman -S --noconfirm "$d"
 		fi
-	done <<-EOF
-	$PRE_BUILD_CMDS
-	EOF
-fi
+	done
+
+	if ! grep -q 'EUID == 69' /usr/bin/makepkg; then
+		# makepkg cannot not as root by default
+		sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+		sed -i \
+			-e 's|-O2|-O3|'                              \
+			-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
+			-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
+			/etc/makepkg.conf
+		cat /etc/makepkg.conf
+	fi
+}
+
+_setup_chaotic_aur() {
+	if ! grep -q '^\[chaotic-aur\]' /etc/pacman.conf; then
+		pacman-key --init
+		pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+		pacman-key --lsign-key 3056513887B78AEB
+		pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+		pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+		echo '[chaotic-aur]' >> /etc/pacman.conf
+		echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+	fi
+}
+
+_install_chaotic_aur_pkg() {
+	_setup_chaotic_aur
+	_info_msg "Adding Chaotic AUR packages: $*"
+	# no -Syu here since it can remove the debloated packages
+	sudo pacman -Sy --noconfirm "$@"
+	exit 0
+}
+
+_get_archlinux_pkgbuild() {
+	git clone --depth 1 https://gitlab.archlinux.org/archlinux/packaging/packages/"$1" ./"$1"
+	cd ./"$1"
+}
+
+_get_aur_pkgbuild() {
+	git clone --depth 1 https://aur.archlinux.org/"$1" ./"$1"
+	cd ./"$1"
+}
+
+_local_pkgbuild() {
+	if [ ! -f "$PWD"/PKGBUILD ]; then
+		>&2 echo "ERROR: No PKGBUILD found in $PWD"
+		exit 1
+	fi
+}
+
+_configure_arch() {
+	if ! grep -q "arch=.*$ARCH" ./PKGBUILD; then
+		sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
+	fi
+}
+
+_run_precmds() {
+	if [ -n "$PRE_BUILD_CMDS" ]; then
+		_info_msg "Running additional pre-build commands..."
+		while IFS= read -r CMD; do
+			if [ -n "$CMD" ]; then
+				_info_msg "Running: $CMD"
+				eval "$CMD"
+			fi
+		done <<-EOF
+		$PRE_BUILD_CMDS
+		EOF
+	fi
+}
+
+_prepare
+
+case "$1" in
+	--chaotic-aur)   shift; _install_chaotic_aur_pkg "$@";;
+	--archlinux-pkg) shift; _get_archlinux_pkgbuild "$@";;
+	'')              _local_pkgbuild ;;
+	*)               _get_aur_pkgbuild "$@";;
+esac
+
+_configure_arch
+_run_precmds
 
 _info_msg "To build:"
 cat ./PKGBUILD
