4034cf8f7e39a8e746bad37f8445110541c54a03
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-22 18:53:59 -0400

    make sure prevent sdl from loading libdecor when not bundled

--- useful-tools/quick-sharun.sh
+++ useful-tools/quick-sharun.sh
@@ -1726,11 +1726,10 @@ for lib do case "$lib" in
 		;;
 	*libSDL*.so*)
 		# SDL may be bundled without libdecor since it maybe missing from the CI runner
-		# or the application makes of GTK/Qt + SDL, in which case we do not need libdecor
-		# at all, make sure SDL does not attempt to load libdecor in these cases
-		ADD_HOOKS="${ADD_HOOKS:+$ADD_HOOKS:}fix-gnome-csd.src.hook"
-		if [ "$DEPLOY_GTK" = 1 ] || [ "$DEPLOY_QT" = 1 ] || [ "$libdecor" != 1 ]; then
-			:> "$APPDIR"/.disable-libdecor
+		# or the application makes use of GTK/Qt + SDL, in which case we do not need 
+		# libdecor at all, make sure SDL does not attempt to load libdecor in these cases
+		if [ "$libdecor" != 1 ]; then
+			sed -i -e 's|libdecor-0.so.0|fuck-gnome.so.0|g' "$lib"
 		fi
 		;;
 	esac
