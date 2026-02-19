b37f305c526c4689e1ba83974317418799b1e70d
crueter <crueter@eden-emu.dev>
2026-02-19 08:56:01 -0500

    add `vulkan-terakan` to vkcube demo (#323)

--- useful-tools/demo/vkcube-glxgears-appimage.sh
+++ useful-tools/demo/vkcube-glxgears-appimage.sh
@@ -6,6 +6,7 @@ set -eux
 
 ARCH="$(uname -m)"
 SHARUN="https://raw.githubusercontent.com/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/refs/heads/main/useful-tools/quick-sharun.sh"
+AUR="https://raw.githubusercontent.com/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/refs/heads/main/useful-tools/make-aur-package.sh"
 EXTRA_PACKAGES="https://raw.githubusercontent.com/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 export DEPLOY_OPENGL=1
@@ -46,6 +47,17 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh --add-mesa --prefer-nano
+
+if [ "$ARCH" = 'x86_64' ]; then
+	# We are experimenting with adding vulkan-terakan
+	# This adds vulkan support for super old radeon gpus
+	# It hasn't been upstreamed to mesa so we have to build it
+	echo "Building vulkan-terakan aur package..."
+	echo "---------------------------------------------------------------"
+	wget --retry-connrefused --tries=30 "$AUR" -O ./make-aur-package.sh
+	chmod +x ./make-aur-package.sh
+	./make-aur-package.sh vulkan-terakan-git
+fi
 
 echo "Bundling AppImage..."
 echo "---------------------------------------------------------------"
