5b1a1ae62d6f34107cf3c5af34813f554824ea7b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-11 02:40:15 -0400

    add `webkit2gtk-4.1-mini`

--- useful-tools/demo/webkit2gtk4-demo-appimage.sh
+++ useful-tools/demo/webkit2gtk4-demo-appimage.sh
@@ -35,7 +35,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+./get-debloated-pkgs.sh --add-common --prefer-nano webkit2gtk-4.1-mini
 
 echo "Building webkit2gtk4-demo..."
 echo "---------------------------------------------------------------"
